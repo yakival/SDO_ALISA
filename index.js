f2e3272bdc24fb14b323c000e4af8116cb384987
yakival <yakival@mail.ru>
2019-07-03 06:38:47 +0300

    make it better

--- index.js
+++ index.js
@@ -38,64 +38,6 @@ db.insert({name : "Boris", year: 1946}, function (error, newDoc) {
 
 }
 
-  if (req.body.request.command == "no text")
-  {
-    res.json({
-      version: req.body.version,
-      session: req.body.session,
-      response: {
-        text: "",
-        end_session: false,
-      },
-    });
-  }
-
-  else if (req.body.request.command == "no version")
-  {
-    res.json({
-      session: req.body.session,
-      response: {
-        text: req.body.request.command || 'Hello!',
-        end_session: false,
-      },
-    });
-  }
-
-  else if (req.body.request.command == "no session")
-  {
-    res.json({
-      version: req.body.version,
-      response: {
-        text: req.body.request.command || 'Hello!',
-        end_session: false,
-      },
-    });
-  }
-
-  else if (req.body.request.command == "end session")
-  {
-    res.json({
-      version: req.body.version,
-      session: req.body.session,
-      response: {
-        text: req.body.request.command || 'Hello!',
-        end_session: true,
-      },
-    });
-  }
-
-  else 
-  {
-    res.json({
-      version: req.body.version,
-      session: req.body.session,
-        response: {
-          text: req.body.request.command || 'Hello!',
-          
-          end_session: false,
-        },
-    })
-  ;}
 });
 
 app.use('*', function (req, res) {
