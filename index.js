cca1139da0caf5b0b80d4255a1b31e915b664a36
yakival <yakival@mail.ru>
2019-07-05 07:34:55 +0300

    make it better

--- index.js
+++ index.js
@@ -102,13 +102,6 @@ app.post('/', function (req, res) {
                             });
                             return;
                         }
-                        client.release();
-                        res.json({version: req.body.version, session: req.body.session, response: {
-                                text: command,
-                                end_session: false,
-                            },
-                        });
-                        return;
 
                         if((command.split(".")<3)||(command.indexOf("http")===-1)){
                             client.release();
